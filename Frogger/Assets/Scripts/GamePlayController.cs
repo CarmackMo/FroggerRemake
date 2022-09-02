11939fdc2c65ba71467ac8fd980c9bcf0885f6fa
u1412161 <116010169@link.cuhk.edu.cn>
2022-09-01 22:54:19 -0600

    Adding timer

---
+++ Frogger/Assets/Scripts/GamePlayController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayController : MonoBehaviour
+{
+
+    public float gameTime = 0f;
+
+
+
+    private void Start()
+    {
+        StartCoroutine(TimerCoroutrine());
+    }
+
+
+
+    IEnumerator TimerCoroutrine()
+    {
+        while (gameTime > 0)
+        {
+            GamePanel.Instance.UpdateTimer((int)gameTime);
+            yield return new WaitForEndOfFrame();
+            gameTime -= Time.deltaTime;
+        }
+
+        yield break;
+
+    }
+    
+
+
+}
--- Frogger/Assets/Scripts/UI/GamePanel.cs
+++ Frogger/Assets/Scripts/UI/GamePanel.cs
@@ -5,9 +5,28 @@ using UnityEngine.UI;
 using TMPro;
 
 public class GamePanel : CanvasSingleton<GamePanel>
-{ 
-    
+{
+    public TextMeshProUGUI timerText;
 
 
+    public void UpdateTimer(int time)
+    {
 
+        int min = time / 60;
+        int sec = time % 60;
+
+        string text = "";
+
+        if (min < 10)
+            text += $"0{min}:";
+        else
+            text += $"{min}:";
+
+        if (sec < 10)
+            text += $"0{sec}";
+        else
+            text += $"{sec}";
+
+        timerText.text = text;
+    }
 }
